b38aaa2f0733f78d60e31e1f816c666503e1fb12
Drew Parker <drewpark88@gmail.com>
2016-06-21 21:57:30 -0500

    First commit

---
+++ app/assets/js/scripts.js
@@ -0,0 +1,17 @@
+/*!
+ * MaterialModal
+ * A simple, clean and efficient modal popup for material design
+ * https://drewparker.me
+ * @author Drew Parker
+ * @version 1.0
+ * Copyright 2016. MIT licensed.
+ */
+(function ($, window, document, undefined) {
+
+  'use strict';
+
+  $(function () {
+    // FastShell
+  });
+
+})(jQuery, window, document);
